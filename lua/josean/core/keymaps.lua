429ab5ebedaed737ffac1ae0e6f2e0ff83b2a8ea
Romel Campbell <romel.campbell@gmail.com>
2025-09-12 04:39:23 -0400

    Command to open a horizontal terminal

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -16,6 +16,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>st", ":split | terminal<CR>", { desc = "Open a horizontal terminal" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
