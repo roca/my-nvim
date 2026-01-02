78e1703785a82366075c298b3153b626a3c800ea
Romel Campbell <romel.campbell@gmail.com>
2026-01-02 15:10:52 -0500

    Emoji plugin

--- lua/josean/plugins/init.lua
+++ lua/josean/plugins/init.lua
@@ -1,5 +1,5 @@
-
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator"-- tmux & split window navigation
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"allaman/emoji.nvim", -- Emoji picker
 }
